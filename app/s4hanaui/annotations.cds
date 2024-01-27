ceccc04fac33c09f465ae6c44c714076da4be83a
riteshanand <ritesh.anand92@gmail.com>
2024-01-27 15:59:48 +0000

    first deployed s4hext

---
+++ app/s4hanaui/annotations.cds
@@ -0,0 +1,1 @@
+using s4hext.srv.CatalogService as service from '../../srv/CatalogService';
---
+++ app/s4hanaui/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("com.s4.s4hanaui.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
